4b6c26b8348f30337f54a0e8a190858be35ff6f7
Fadonougbo <fadougbogautier@gmail.com>
2025-04-18 18:48:10 +0100

    update

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,8 @@
 |
 */
 
+import logger from '@adonisjs/core/services/logger'
 import router from '@adonisjs/core/services/router'
 
+logger.info('app test okok')
 router.on('/').render('pages/home')
